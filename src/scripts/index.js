9e5dd16f4f1d85a7b5bb863d148e3f480c885efc
GarrafaTati <tatiigarrafa@gmail.com>
2023-03-09 14:38:38 -0300

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+// import "./styles/index.css";
+let someStr = "Eu programei. Eu salvei. Eu empacotei.";
+
+const numbers = [2, 3, 5];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+console.log(doubledNumbers);
