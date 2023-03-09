7eb9ba9bc07d3482f6e57524c885da519ee90e76
GarrafaTati <tatiigarrafa@gmail.com>
2023-03-09 15:55:13 -0300

    webpack config

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-// import "./styles/index.css";
-let someStr = "Eu programei. Eu salvei. Eu empacotei.";
-
-const numbers = [2, 3, 5];
-
-const doubledNumbers = numbers.map((number) => number * 2);
-
-console.log(doubledNumbers);
