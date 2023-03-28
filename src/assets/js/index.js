d212a46211185fb053a53c2827ec620ee0126c96
GarrafaTati <tatiigarrafa@gmail.com>
2023-03-28 09:33:36 -0300

    timeline css, skills bar, typo

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,22 @@
 import "./timeline.js";
 import "../styles/index.scss";
+
+const skillsContainer = document.querySelector(".skills");
+
+function isScrolledIntoView(elem) {
+  var rect = elem.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+  return isVisible;
+}
+
+window.addEventListener("scroll", function () {
+  if (isScrolledIntoView(skillsContainer)) {
+    skillsContainer.classList.add("inView");
+  } else {
+    skillsContainer.classList.remove("inView");
+  }
+});
